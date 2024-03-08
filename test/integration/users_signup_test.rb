4bb964b2b79c3ba4ffd357816517c4e7a649f114
SatoKosuke1006 <162126882+SatoKosuke1006@users.noreply.github.com>
2024-03-08 15:26:04 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,8 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_response :unprocessable_entity
-    assert_template 'users/new'
+    assert_template 'users/show'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
+    assert_not flash.empty?
   end
 end
