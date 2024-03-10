a1824e76669bb0fcc81422a7c29d702243e3e48b
SatoKosuke1006 <162126882+SatoKosuke1006@users.noreply.github.com>
2024-03-10 08:31:27 +0000

    Finish user edit, update, index, and destroy actions

--- db/migrate/20240309081741_add_remember_digest_to_users.rb
+++ db/migrate/20240309081741_add_remember_digest_to_users.rb
@@ -1,22 +1,5 @@
-class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-
-  # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # ランダムなトークンを返す
-  def User.new_token
-    SecureRandom.urlsafe_base64
+class AddRememberDigestToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :remember_digest, :string
   end
 end
