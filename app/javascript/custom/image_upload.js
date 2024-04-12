52584cb5f463d4d95363bc608e15664b847470c9
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-04-12 15:23:34 +0900

    最初の閲覧からプレビューが見れるように修正

--- app/javascript/custom/image_upload.js
+++ app/javascript/custom/image_upload.js
@@ -1,21 +1,21 @@
 // 巨大画像のアップロードを防止する
 document.addEventListener("turbo:load", function() {
-  document.addEventListener("change", function(event) {
-    let image_upload = document.querySelector('#file-upload');
-    const size_in_megabytes = image_upload.files[0].size/1024/1024;
-    if (size_in_megabytes > 5) {
-      alert("Maximum file size is 5MB. Please choose a smaller file.");
-      image_upload.value = "";
-    }
-  });
+  let image_upload = document.querySelector('#file-upload');
+  if (image_upload) {
+    image_upload.addEventListener("change", function(event) {
+      const size_in_megabytes = this.files[0].size/1024/1024;
+      if (size_in_megabytes > 5) {
+        alert("Maximum file size is 5MB. Please choose a smaller file.");
+        this.value = ""; // Reset the file input
+      } else {
+        // プレビュー画像を表示する
+        var reader = new FileReader();
+        reader.onload = function(e) {
+          document.getElementById("preview-image").src = e.target.result;
+          document.getElementById("preview-image").style.display = "inline";
+        };
+        reader.readAsDataURL(event.target.files[0]);
+      }
+    });
+  }
 });
-
-// プレビュー画像を表示する
-document.getElementById("file-upload").onchange = function(e) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      document.getElementById("preview-image").src = e.target.result;
-      document.getElementById("preview-image").style.display = "inline";
-    };
-    reader.readAsDataURL(this.files[0]);
-  };
