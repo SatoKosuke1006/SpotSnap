4fb5166588feb9a6667a3a80f63050cc6e285820
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-06-07 17:46:01 +0900

    デプロイ自動化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Force SSL to be disabled
+  config.force_ssl = false
+
   pf_domain = ENV['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']
   config.action_dispatch.default_headers = {
     'X-Frame-Options' => "ALLOW-FROM #{pf_domain}"
