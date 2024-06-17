73ca4b15c45ad52aafdeb94280cb43489268a82c
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-06-17 21:47:57 +0900

    エラー対応

--- app/javascript/custom/map.js
+++ app/javascript/custom/map.js
@@ -12,6 +12,7 @@ document.addEventListener("turbo:render", initialize);
 
 // マップを初期化する関数
 function initialize() {
+  console.log("A");
   if (typeof google !== 'undefined' && (document.getElementById('map'))) {
     placesService = new google.maps.places.PlacesService(document.createElement('div')); 
     initMap();
@@ -26,6 +27,7 @@ function initialize() {
 
 // マップの初期設定
 function initMap() {
+  console.log("B");
   const mapElement = document.getElementById('map');
   if (!navigator.geolocation) return;
 
@@ -45,6 +47,7 @@ function initMap() {
     geocoder = new google.maps.Geocoder();
     infowindow = new google.maps.InfoWindow();
 
+    console.log("C");
     fetch(`/location_posts/count?lat=${userSpecifiedLocation.lat}&lng=${userSpecifiedLocation.lng}`)
       .then(response => response.json())
       .then(data => {
@@ -60,6 +63,7 @@ function initMap() {
 // ユーザー指定の位置情報を取得する関数
 function getUserSpecifiedLocation(mapElement, defaultLat, defaultLng) {
   return new Promise((resolve, reject) => {
+    console.log("D");
     if (mapElement && mapElement.hasAttribute('data-place-id')) {
       const placeId = mapElement.getAttribute('data-place-id');
       const request = {
@@ -83,6 +87,7 @@ function getUserSpecifiedLocation(mapElement, defaultLat, defaultLng) {
 
 // マーカーを設定する数
 function createDraggableMarker(location, postCount) {
+  console.log("E");
   const fillColor = postCount > 0 ? "#FFA500" : "#00FF00"; 
   const strokeColor = postCount > 0 ? "#FFA500" : "#00FF00"; 
 
@@ -130,6 +135,7 @@ function displayLocation(marker) {
 
 // 入力れた住所から位置を検索し、マップを更新する関数
 function codeAddress() {
+  console.log("F");
   const inputAddress = document.getElementById('address').value;
   const request = {
     query: inputAddress,
@@ -183,6 +189,7 @@ function updateInputFields(placeId) {
 
 // オートコンプリート機能を追加する関数
 function enableAutocomplete() {
+    console.log("G");
     const input = document.getElementById('address');
     const autocomplete = new google.maps.places.Autocomplete(input);
 
