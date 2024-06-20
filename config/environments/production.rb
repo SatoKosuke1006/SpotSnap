c9a592a46a58d9f119b54f1cae4a4cc235ddb2ba
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-06-20 15:54:41 +0900

    本番環境　メール送信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://spotsnap.net'
   config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_options = { from: 'sato1006kousuke@yahoo.co.jp' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'email-smtp.us-east-1.amazonaws.com',
     :port           => 587,
