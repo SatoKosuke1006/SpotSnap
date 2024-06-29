74e09f5a63eb995df54d0d9f2bcce6aa67a217f1
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-06-29 13:47:44 +0900

    メール送信対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address        => 'email-smtp.us-east-1.amazonaws.com',
     :port           => 587,
-    :user_name      => 'AKIAZI2LD2LJTPEPKH74',
-    :password       => 'BNAaPJQkP4A1eCyXxl5dLr34ChzA5lw/INLzd8y/ZB5Q',
+    :user_name      => ENV['SMTP_USERNAME'],
+    :password       => ENV['SMTP_PASSWORD'],
     :authentication => :login,
     :enable_starttls_auto => true
   }
