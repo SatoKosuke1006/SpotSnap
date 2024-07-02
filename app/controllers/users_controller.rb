e5e285a03be5f774e7f8d0d0512ad54cfdda24be
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-07-02 21:39:16 +0900

    エラー対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 # ApplicationController
 class UsersController < ApplicationController
-  before_action :logged_in_user 
+  before_action :logged_in_user, only: %i[index show edit update destroy following followers]
   before_action :correct_user,   only: %i[edit update]
   before_action :admin_user,     only: :destroy
 
