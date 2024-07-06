17f8449d4aefbc580ce42cce3b3bf248b11e5955
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-07-06 20:34:17 +0900

    ホーム画面リダイレクト時のturbo無効化

--- app/javascript/custom/reload.js
+++ app/javascript/custom/reload.js
@@ -9,10 +9,4 @@ document.addEventListener("turbo:load", function() {
   if (performance.getEntriesByType("navigation")[0].type === "back_forward") {
     window.location.reload();
   }
-
-//   // ホーム画面であればリロード
-//   if (window.location.pathname === '/home' && !window.location.search.includes('reloaded=true')) {
-//     const newUrl = window.location.pathname + window.location.search + (window.location.search ? '&' : '?') + 'reloaded=true';
-//     window.location.replace(newUrl);
-//   }
 });
--- app/javascript/custom/toggle_content.js
+++ app/javascript/custom/toggle_content.js
@@ -4,16 +4,20 @@ document.addEventListener("turbo:render", setupToggleContent);
 
 function setupToggleContent() {
   document.querySelectorAll('.toggle-content-btn').forEach(button => {
-    button.addEventListener('click', () => {
-      const content = button.nextElementSibling;
-      const arrow = button.querySelector('.toggle-arrow');
-      if (content.style.display === 'none') {
-        content.style.display = 'block';
-        arrow.textContent = '▲';
-      } else {
-        content.style.display = 'none';
-        arrow.textContent = '▼';
-      }
-    });
+    button.removeEventListener('click', toggleContent); // 既存のイベントリスナーを削除
+    button.addEventListener('click', toggleContent); // 新しいイベントリスナーを追加
   });
+}
+
+function toggleContent(event) {
+  const button = event.currentTarget;
+  const content = button.nextElementSibling;
+  const arrow = button.querySelector('.toggle-arrow');
+  if (content.style.display === 'none') {
+    content.style.display = 'block';
+    arrow.textContent = '▲';
+  } else {
+    content.style.display = 'none';
+    arrow.textContent = '▼';
+  }
 }
