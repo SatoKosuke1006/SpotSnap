b94f374336877b8310bae5bffb79d6551e1e1890
Kosuke <sato1006kousuke@yahoo.co.jp>
2024-07-21 21:10:19 +0900

    ヘッダー追加

--- app/javascript/custom/toggle_content.js
+++ app/javascript/custom/toggle_content.js
@@ -1,11 +1,12 @@
+//　投稿内の説明文の表示/非表示切り替え
 document.addEventListener("turbo:load", setupToggleContent);
 document.addEventListener("turbo:frame-load", setupToggleContent);
 document.addEventListener("turbo:render", setupToggleContent);
 
 function setupToggleContent() {
   document.querySelectorAll('.toggle-content-btn').forEach(button => {
-    button.removeEventListener('click', toggleContent); // 既存のイベントリスナーを削除
-    button.addEventListener('click', toggleContent); // 新しいイベントリスナーを追加
+    button.removeEventListener('click', toggleContent); 
+    button.addEventListener('click', toggleContent); 
   });
 }
 
@@ -24,3 +25,20 @@ function toggleContent(event) {
     placeName.style.display = 'inline';
   }
 }
+
+// サイドバーの表示/非表示切り替え
+document.addEventListener("turbo:load", function() {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const sidebar = document.querySelector('.sidebar');
+  const overlay = document.querySelector('.overlay');
+
+  hamburgerMenu.addEventListener('click', function() {
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+
+  overlay.addEventListener('click', function() {
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+  });
+});
